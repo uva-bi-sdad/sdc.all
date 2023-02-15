bd34dc4c85bf81b2af386357391f295af8eb62df
uva-bi-sdad <>
2023-02-15 16:02:48 +0000

    Update from https://github.com/uva-bi-sdad/sdc.all_dev/commit/9010c2d32f35ed2e35f3304a780ac285054c21c3

---
+++ code/inventory/geographies_list.py
@@ -0,0 +1,106 @@
+import os
+import logging
+from pathlib import Path
+from enum import Enum
+import pandas as pd
+from datetime import datetime
+import json
+import urllib.request
+from string import Template
+import traceback
+import re
+import geopandas as gpd
+
+
+def evaluate_folder(dirpath):
+    report = ""
+            
+    #
+    # CREATE GEOGRAPHIES DF -------------------------------------
+    #
+
+    geographies_df = pd.DataFrame(columns = ['geoid', 'region_name', 'region_type', 'year'])
+    
+    # LOOP THROUGH SDC.GEOGRAPHIES REPO  ---------------------
+    
+    for dir in os.listdir(dirpath):
+        subdir = os.path.join(dirpath, dir)
+        if not os.path.isdir(subdir):
+            continue
+        report += "<h3> %s </h3>\n" % (dir)
+        
+        # CHECK EACH DATA/DISTRIBUTION FILE ------------------------
+        
+        for path in Path(subdir).rglob("data/distribution/**/*"):
+            logging.debug("\tEvaluating: %s" % path.name)
+
+            if not os.path.isfile(path):
+                # if path is not a file, skip to the next file to check
+                continue
+            
+            full_path = path.name
+
+            if path.suffix in [".geojson"]:                
+               
+                try:
+                    # KRISTIAN - change this line to read in a geojson
+                    path = str(path)
+                    df = gpd.read_file(path, ignore_geometry=True)
+                    
+                    # KRISTIAN FILL IN CODE HERE - remove geographies column of df
+                    df = pd.DataFrame(df)
+        
+                    # update geographies df --------------------
+                    geographies_df = pd.concat([geographies_df, df], ignore_index = True) 
+                    
+                    report += "\t<p><font color='green'> [ADDED TO CSV] </font> %s%s</p>\n" % (dirpath, full_path)
+                    
+                except:
+                    print(traceback.format_exc())
+                    report += "\t<p><font color='red'> [ERROR] </font> %s/%s</p>\n" % (dirpath, full_path)
+                       
+    # deduplicate rows of geographies df  
+    # KRISTIAN FILL IN
+    final_geographies_df = geographies_df.drop_duplicates()
+            
+    return report, final_geographies_df
+
+
+if __name__ == "__main__":
+
+    # with urllib.request.urlopen(
+    #    "https://raw.githubusercontent.com/uva-bi-sdad/data_repo_structure/main/col_names.json"
+    # ) as url:
+    #    req_cols = json.load(url)
+
+    # req_cols = set(req_cols)
+    # print(req_cols)
+
+    report, inv_final_df = evaluate_folder("./data/sdc.geographies")
+    time_checked = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    print(time_checked)
+
+    t = Template(
+        """
+    <html>
+        <head>
+        <title>Data Inventory Report</title>
+        </head>
+        <body>
+            Last updated: $time_checked
+            $report
+        </body>
+    </html>
+    """
+    )
+    
+    # write report
+    print(t.substitute(time_checked=time_checked, report=report))
+    
+    #with open("./docs/geographies_report.html", "w") as f:
+    with open("./docs/geographies_report.html", "w") as f:
+        f.write(t.substitute(time_checked=time_checked, report=report))
+        
+    # write inventory    
+    inv_final_df.to_csv("./docs/geographies.csv", index=False)
