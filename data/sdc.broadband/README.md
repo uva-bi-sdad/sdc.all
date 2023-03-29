9906bf0c3ebb9a754270f9bf14c3763826c9b60a
uva-bi-sdad <>
2023-03-29 09:05:47 +0000

    Update from https://github.com/uva-bi-sdad/sdc.all_dev/commit/b038d217e18723e47a30665d9ae4ef1339edb952

--- data/sdc.broadband/README.md
+++ data/sdc.broadband/README.md
@@ -1,5 +1,8 @@
 # sdc.broadband-dev
 
+## Description of the repository
+  - The dashboard branch is where the pages are referencing
+
 ## Description on how the measure is selected
   - We look for the cheapest plan above 100 Mb / second download speed
   - For each block, we randomly select an address in the block
---
+++ data/sdc.food/Food Access/Food Items/code/distribution/ingest01.R
@@ -0,0 +1,7 @@
+## DOWNLOAD SNAP HISTORICAL RETAIL LOCATOR DATA
+
+url <- "https://www.fns.usda.gov/sites/default/files/resource-files/Historical-SNAP-Retailer-Locator-Data-20211231.zip"
+destfile <- "./Food Access/Supermarket Drive Times/data/original/Historical-SNAP-Retailer-Locator-Data-20211231.zip"
+download.file(url = url, destfile = destfile)
+
+
