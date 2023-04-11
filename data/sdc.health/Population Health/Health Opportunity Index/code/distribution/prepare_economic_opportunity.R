2d854d250c372eaf3fe74db812398249f8267c0c
uva-bi-sdad <>
2023-04-11 09:07:05 +0000

    Update from https://github.com/uva-bi-sdad/sdc.all_dev/commit/eae192b9ea76c6bda729ddfdc4675e27bbf818a6

--- data/sdc.health/Population Health/Health Opportunity Index/code/distribution/prepare_economic_opportunity.R
+++ data/sdc.health/Population Health/Health Opportunity Index/code/distribution/prepare_economic_opportunity.R
@@ -1,68 +1,66 @@
-# Replicating Economic Opportunity Profile of the VHD's HOI
-# The Economic Opportunity Profile examines the impact place has on each member’s ability 
-# to participate in the economic life of a community.  Factors influencing economic 
-# opportunity include access to jobs, labor participation rates, the distribution of 
-# income within a community. 
-# https://apps.vdh.virginia.gov/omhhe/hoi/economic-opportunity-profile
-
-# packages
 library(readxl)
-library(dplyr)
-library(tidyverse)
+library(data.table)
 library(sf)
-library(reshape2)
+library(fabricatr)
 
-# data from HOI website
-orig_df <- read_excel("Population Health/Health Opportunity Index/data/original/econ_opp.xlsx")
-df_tracts <- orig_df[,c("County Name", "LHD Name", "STFIPS (CountyHOI)", 
-                        "Ctfips", "Indicator Selector")] 
-df_tracts <- df_tracts %>% filter(is.na(`Indicator Selector`) == FALSE)
-# assign quintiles
-df_tracts <- df_tracts %>% mutate(
-  quintiles = case_when(
-    `Indicator Selector` == "Very Low" ~ 1, 
-    `Indicator Selector` == "Low" ~ 2,
-    `Indicator Selector` == "Average" ~ 3,
-    `Indicator Selector` == "High" ~ 4,
-    `Indicator Selector` == "Very High" ~ 5
-  )
-)
-df_tracts["new_id"] <- paste0(df_tracts$Ctfips, "_", df_tracts$quintiles)
-# remove duplicates
-df_tracts_nodups <- df_tracts %>% distinct()
-# check if the census tarct ID is unique
-# df_tracts_unq <- df_tracts_nodups %>% distinct(Ctfips, .keep_all = TRUE) # it is unique
+# Data
+econ_2017 <- setDT(read_excel("Population Health/Health Opportunity Index/data/original/econ_opp.xlsx", sheet = 1))
+hoi_2022 <- setDT(read_excel("Population Health/Health Opportunity Index/data/original/hoi_indexes_quintile_2022.xlsx", sheet = 1))
+# Geographies
+geo_names <- fread("https://raw.githubusercontent.com/uva-bi-sdad/sdc.metadata/master/geographies.csv")
+geo_names <- geo_names[!region_name %like% "District Of Columbia" & 
+                         !region_name %like% " city" &
+                         !region_name %like% " of " &
+                         !region_name %like% " and "]
+geo_names <- geo_names[geo_names$geoid %like% "^51",]
+geo_names_10 <- geo_names[year == "2010", .(geoid, region_name, region_type)]
+geo_names_20 <- geo_names[year == "2020", .(geoid, region_name, region_type)]
 
-# rename measures
-out_df <- df_tracts_nodups %>% 
-  rename( "geoid"= "Ctfips",
-          "economic_opportunity_indicator" = "quintiles")
-out_df <- out_df[,c("geoid", "economic_opportunity_indicator")]
+# 2017
+econ_2017_sel <-
+  econ_2017[, .(
+    geoid = Ctfips,
+    measure = "economic_opportunity_indicator",
+    measure_type = "index",
+    value = `Indicator Selector`,
+    year = "2017",
+    moe = ""
+  )]
+
+econ_2017_sel[value == "Very Low", value := "1"]
+econ_2017_sel[value == "Low", value := "2"]
+econ_2017_sel[value == "Average", value := "3"]
+econ_2017_sel[value == "High", value := "4"]
+econ_2017_sel[value == "Very High", value := "5"]
+econ_2017_sel[, value := as.integer(value)]
+
+econ_2017_sel <- unique(econ_2017_sel)
+
+# merge with geo
+econ_2017_sel_mrg <- merge(econ_2017_sel, geo_names_10, by = "geoid")
+econ_2017_sel_mrg <- econ_2017_sel_mrg[, .(geoid, measure, measure_type, region_name, region_type, value, year, moe)]
 
 
-# geographies
-geos_data <- st_read("https://raw.githubusercontent.com/uva-bi-sdad/sdc.geographies/main/VA/Census%20Geographies/Tract/2010/data/distribution/va_geo_census_cb_2010_census_tracts.geojson") %>%
-  select(geoid, region_name, region_type)
+# 2022
+econ_2022_sel <- hoi_2022[,.(geoid = CT, 
+                             measure = "economic_opportunity_indicator",
+                             measure_type = "index",
+                             value = split_quantile(hoi_2022$`Economic Profile SI`, 5),
+                             year = "2022",
+                             moe = "")]
+            
+econ_2022_sel[, value := as.integer(value)]
+econ_2022_sel <- unique(econ_2022_sel)
 
-geos_data$geometry <- NULL
+# merge with geo
+econ_2022_sel_mrg <- merge(econ_2022_sel, geo_names_20, by = "geoid")
+econ_2022_sel_mrg <- econ_2022_sel_mrg[, .(geoid, measure, measure_type, region_name, region_type, value, year, moe)]
 
-# add geographies
-out_df <- left_join(out_df, geos_data, by=c("geoid"))
 
-# long format
-out_long <- melt(out_df,
-                 id.vars=c("geoid", "region_type", "region_name"),
-                 variable.name="measure",
-                 value.name="value"
-)
+# combine
+econ_sel_mrg  <- rbindlist(list(econ_2017_sel_mrg, econ_2022_sel_mrg))
 
-# add missing columns
-out_long["year"] <- "2017"
-out_long["measure_type"] <- "index"
-out_long["moe"] <- ""
 
-# Select final columns
-out_long <- out_long[, c("geoid", "region_name", "region_type", "year", "measure", "value", "measure_type", "moe")]
 
-# save the dataset 
-write_csv(out_long, xzfile("Population Health/Health Opportunity Index/data/distribution/va_tr_vdh_2017_economic_opportunity_profile.csv.xz", compression = 9))
+fwrite(econ_sel_mrg, "Population Health/Health Opportunity Index/data/distribution/va_cttr_vdh_2017_2022_economic_opportunity_profile.csv")
+
