701a68d38a3f3e417a841c8087e5480180fa60d8
uva-bi-sdad <>
2023-04-21 19:22:21 +0000

    Update from https://github.com/uva-bi-sdad/sdc.all_dev/commit/8535cdcd7e158d6ba056779f68f54204c96354c8

--- code/inventory/geographies_list.py
+++ code/inventory/geographies_list.py
@@ -43,11 +43,11 @@ def evaluate_folder(dirpath):
             if path.suffix in [".geojson"]:                
                
                 try:
-                    # KRISTIAN - change this line to read in a geojson
+                    # read in a geojson
                     path = str(path)
                     df = gpd.read_file(path, ignore_geometry=True)
                     
-                    # KRISTIAN FILL IN CODE HERE - remove geographies column of df
+                    # remove geographies column of df
                     df = pd.DataFrame(df)
         
                     # update geographies df --------------------
@@ -58,9 +58,13 @@ def evaluate_folder(dirpath):
                 except:
                     print(traceback.format_exc())
                     report += "\t<p><font color='red'> [ERROR] </font> %s/%s</p>\n" % (dirpath, full_path)
-                       
+           
+    # change formatting of "And" to "and", "Of" to "of" and "city" to "City" 
+    geographies_df['region_name'] = geographies_df['region_name'].str.replace(' Of ',' of ')
+    geographies_df['region_name'] = geographies_df['region_name'].str.replace(' And ',' and ')
+    geographies_df['region_name'] = geographies_df['region_name'].str.replace('city','City')
+    
     # deduplicate rows of geographies df  
-    # KRISTIAN FILL IN
     final_geographies_df = geographies_df.drop_duplicates()
             
     return report, final_geographies_df
@@ -98,9 +102,8 @@ if __name__ == "__main__":
     # write report
     print(t.substitute(time_checked=time_checked, report=report))
     
-    #with open("./docs/geographies_report.html", "w") as f:
     with open("./docs/geographies_report.html", "w") as f:
         f.write(t.substitute(time_checked=time_checked, report=report))
         
     # write inventory    
-    inv_final_df.to_csv("./docs/geographies.csv", index=False)
+    inv_final_df.to_csv("./docs/geographies_metadata.csv", index=False)
