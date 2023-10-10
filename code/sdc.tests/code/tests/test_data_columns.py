3e38bf6d478db1c8dadfe4bfa01740ede86d9d9a
uva-bi-sdad <>
2023-10-10 17:01:55 +0000

    Update from https://github.com/uva-bi-sdad/sdc.all_dev/commit/f7f279f4e85779bf8f43150a16d81a7c8f912588

--- code/sdc.tests/code/tests/test_data_columns.py
+++ code/sdc.tests/code/tests/test_data_columns.py
@@ -10,49 +10,65 @@ from string import Template
 import traceback
 from pytz import timezone
 from test import Test
+import re
 
 
 def evaluate_folder(req_cols, dirpath):
     report = ""
 
+    # LOOP THROUGH EACH REPO (SDC. ...) ---------------------
+    
     for dir in os.listdir(dirpath):
         subdir = os.path.join(dirpath, dir)
         if not os.path.isdir(subdir):
             continue
         report += "<h3> %s </h3>\n" % (dir)
-        for path in sorted(Path(subdir).rglob("distribution/**/*")):
+        
+        if dir == "sdc.geographies":
+            report += "NA"
+            continue
+        
+        # CHECK EACH DATA/DISTRIBUTION FOLDER ------------------------
+        
+        for path in sorted(Path(subdir).rglob("data/distribution")):
+            
             logging.debug("\tEvaluating: %s" % path.name)
 
-            if not os.path.isfile(path):
-                # if path is not a file, skip to the next file to check
-                continue
+            # get path info
+            
+            try:    
+                parent_dir = str(path).split('sdc.').pop()
+                parent_dir = re.search('/(.*?)/data/distribution', parent_dir).group(1)
+            except:
+                parent_dir = "PARENT_DIRECTORY"
 
-            parent_dir = path.parent
+            # LOOP THROUGH EACH DATA FILE IN FOLDER -----------------
+            
+            for f in path.iterdir():
+            
+                if not os.path.isfile(f):
+                    # if path is not a file, skip to the next file to check
+                    continue
+            
+                if f.suffix in [".xz", ".csv"]:
+                
+                    try:
+                        df = pd.read_csv(f.resolve())
+                        cols = set(df.columns)
+                        missing_cols = list(req_cols.difference(cols))
+                        extra_cols = list(cols.difference(req_cols))
 
-            if path.suffix in [".xz", ".csv"]:
-                full_path = path.name
-                try:
-                    df = pd.read_csv(path.resolve())
-                    cols = set(df.columns)
-                    # is_valid = len(cols.intersection(req_cols)) == len(
-                    #     req_cols
-                    # ) and len(df.columns) == len(req_cols)
-                    is_valid = len(req_cols - cols) == 0
-
-                    if is_valid:
-                        report += "\t<p><font color='#009E73'> [VALID] </font> %s</p>\n" % (full_path)
-                    elif len(cols.intersection(req_cols)) > 1:
-                        report += "\t<p><font color='#D55E00'> [MISSING %s] </font> Needs: %s, Has: %s, %s </p>\n" % (
-                            len(req_cols - cols),
-                            req_cols - cols,
-                            cols,
-                            full_path,
-                        )
-                    else:
-                        report += "\t<p>[NO OVERLAP] %s</p>\n" % (full_path)
-                except:
-                    print(traceback.format_exc())
-                    report += "\t<p><font color='#D55E00'> [ERROR] </font> %s</p>\n" % (full_path)
+                        if len(missing_cols) > 0:
+                            report += "\t<p><font color='#D55E00'> [MISSING COLUMNS] </font> %s %s: %s </p>\n" % (missing_cols, parent_dir, f.name)
+                        if len(extra_cols) > 0:
+                            report += "\t<p><font color='#0072B2'> [EXTRA COLUMNS] </font> %s %s: %s </p>\n" % (extra_cols, parent_dir, f.name)
+                        
+                        if (len(missing_cols) + len(extra_cols) == 0):
+                            report += "\t<p><font color='#009E73'> [VALID] </font> %s: %s</p>\n" % (parent_dir, f.name)
+   
+                    except:
+                        print(traceback.format_exc())
+                        report += "\t<p><font color='#D55E00'> [ERROR] </font> %s: %s</p>\n" % (parent_dir, f.name)
     return report
 
 
